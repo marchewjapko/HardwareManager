03a802c523bc80bfe84dc43a7d5e28eb8672d265
marchewjapko <marchewjapko@gmail.com>
2022-11-01 19:56:16 +0100

    Update HardwareMonitorLinux.cs

--- DataSource/Usage/Linux/HardwareMonitorLinux.cs
+++ DataSource/Usage/Linux/HardwareMonitorLinux.cs
@@ -23,37 +23,20 @@ namespace DataSource.Usage.Linux
 
         public UsageDTO GetSystemUsage()
         {
-            float totalUsage = 0;
-            List<(string, float)> perCoreUsage = new();
-            List<(string, float)> diskUsage = new();
-            float memoryUsage = 0;
-            List<(string, float)> bytesReceived = new();
-            List<(string, float)> bytesSent = new();
-            float systemUptime = 0;
-
             Parallel.Invoke(
                 () => { cpuInfo.UpdateCpuReadingsLinux(); },
                 () => { networkInfo.UpdateNetworkReadingsLinux(); },
                 () => { diskInfo.UpdateDiskReadingsLinux(); }
             );
-            Parallel.Invoke(
-               () => { totalUsage = cpuInfo.GetCpuTotalUsage(); },
-               () => { perCoreUsage = cpuInfo.GetCpuPerCoreUsage(); },
-               () => { diskUsage = diskInfo.GetDiskUsage(); },
-               () => { memoryUsage = memoryInfo.GetRemainingMemory(); },
-               () => { bytesReceived = networkInfo.GetBytesReceived(); },
-               () => { bytesSent = networkInfo.GetBytesSent(); },
-               () => { systemUptime = systemInfo.GetSystemUptime(); }
-            );
 
             return new UsageDTO(
-               totalUsage,
-               perCoreUsage,
-               diskUsage,
-               memoryUsage,
-               bytesReceived,
-               bytesSent,
-               systemUptime
+                cpuInfo.GetCpuTotalUsage(),
+                cpuInfo.GetCpuPerCoreUsage(),
+                diskInfo.GetDiskUsage(),
+                memoryInfo.GetRemainingMemory(),
+                networkInfo.GetBytesReceived(),
+                networkInfo.GetBytesSent(),
+                systemInfo.GetSystemUptime()
             );
         }
     }
